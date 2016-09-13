ac5854af278663961b1734d7be486d0d1b15a66b
RonnyXD <meshin.andrey@yandex.ru>
2016-09-13 22:27:11 +0600

    Database
    Database

---
+++ server/js/lib/database.js
@@ -0,0 +1,33 @@
+var Database = function (host, user, password, database) {
+
+    var mysql      = require('mysql');
+
+    var connection = mysql.createConnection({
+        host     : host,
+        user     : user,
+        password : password,
+        database : database
+    });
+
+    connection.connect();
+}
+
+Database.prototype.Query = function (query) {
+
+    connection.query(query, function(err, rows, fields) {
+        if (err) throw err;
+
+        return rows;
+    });
+
+}
+
+Database.prototype.GetById = function (table, id) {
+
+    connection.query('SELECT * FROM '+table+' WHERE id = '+id, function(err, rows, fields) {
+        if (err) throw err;
+
+        return rows;
+    });
+
+}
